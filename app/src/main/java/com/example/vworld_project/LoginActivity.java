2625a4768969a6933790b4e8692595224526734b
samijihd <khwairehsami@gmail.com>
2020-02-19 23:57:48 +0300

    design login page

--- app/src/main/java/com/example/vworld_project/LoginActivity.java
+++ app/src/main/java/com/example/vworld_project/LoginActivity.java
@@ -2,13 +2,42 @@ package com.example.vworld_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ImageView back;
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        back = findViewById(R.id.back_ID);
+        register = findViewById(R.id.register_ID);
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this , MainActivity.class));
+                finish();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this , RegisterActivity.class));
+                finish();
+            }
+        });
     }
 }
