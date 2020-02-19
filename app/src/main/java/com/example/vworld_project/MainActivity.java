69ce045b29d1ab514e3cb05e9049e1beabddbe9d
samijihd <khwairehsami@gmail.com>
2020-02-19 22:43:26 +0300

    design main activity

--- app/src/main/java/com/example/vworld_project/MainActivity.java
+++ app/src/main/java/com/example/vworld_project/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.vworld_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnlogin;
+
+    private TextView btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnlogin = findViewById(R.id.login_ID);
+        btnRegister = findViewById(R.id.register_ID);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , LoginActivity.class));
+                finish();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , RegisterActivity.class));
+                finish();
+            }
+        });
     }
 }
