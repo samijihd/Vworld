4a73d77b19c52ea7076cc5de23c7eaff87bff1ec
samijihd <khwairehsami@gmail.com>
2020-02-20 01:31:30 +0300

    add cmnts

--- app/src/main/java/com/example/vworld_project/RegisterActivity.java
+++ app/src/main/java/com/example/vworld_project/RegisterActivity.java
@@ -2,13 +2,39 @@ package com.example.vworld_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ImageView back;
+    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        back = findViewById(R.id.back_ID);
+        login = findViewById(R.id.loginlink_ID);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this , MainActivity.class));
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this , LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
