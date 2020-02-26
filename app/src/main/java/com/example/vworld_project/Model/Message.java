bb5767b3601e7a0f7493d439631bc79f7c452478
samijihd <khwairehsami@gmail.com>
2020-02-26 09:45:36 +0300

    edit design of login and register

---
+++ app/src/main/java/com/example/vworld_project/Model/Message.java
@@ -0,0 +1,21 @@
+package com.example.vworld_project.Model;
+
+public class Message {
+
+    private String message;
+    private String sender;
+    private String reciver;
+
+    public Message(String message, String sender, String reciver){
+        this.message = message;
+        this.sender = sender;
+        this.reciver = reciver;
+    }
+
+    public Message(){
+
+    }
+
+
+
+}
